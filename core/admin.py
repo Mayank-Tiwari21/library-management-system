17b04d4cf2fa592ab27081374f20d85227db7539
Mayank-Tiwari21 <mayank.tiwari@tothenew.com>
2025-06-05 15:44:41 +0530

    9th_commit:Created app 'users' ,changed 'Reservations->Reservation' in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Book,BookCopy, BorrowTransaction,Reservations
+from .models import CustomUser, Book,BookCopy, BorrowTransaction,Reservation
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 admin.site.register(CustomUser,UserAdmin)
 admin.site.register(Book)
 admin.site.register(BookCopy)
-admin.site.register(Reservations)
+admin.site.register(Reservation)
 admin.site.register(BorrowTransaction)
