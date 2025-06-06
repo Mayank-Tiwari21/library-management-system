7c5b475d2fdcf0921f4e0474002eed8c2079f0ad
Mayank-Tiwari21 <mayank.tiwari@tothenew.com>
2025-06-06 10:03:42 +0530

    12th_commit: views.py:register_view added, users/urls.py updated, lib-mn/urls.py updated, templates/users/register.html added

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .forms import CustomAuthenticationForm
 app_name = "users"
 urlpatterns = [
-    path("login/",auth_views.LoginView.as_view(template_name ="user/login.html"),name = 'login'),
+    path("login/",auth_views.LoginView.as_view(template_name ="user/login.html",authentication_form = CustomAuthenticationForm),name = 'login'),
     path("logout/",auth_views.LogoutView.as_view(next_page = "users:login"),name ="logout"),
-    path("register/",views.register,name = "register")
+    path("register/",views.register_view,name = "register")
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def register_view(request):
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user =form.save()
+            login(request, user)
+            messages.success(request,"Account created sccessfully.")
+            return redirect('core:user_dashboard')
+    else:
+        form = CustomUserCreationForm()
+        return render(request,"users/register.html",{'form':form })
